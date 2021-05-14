ebb4adb6ce25804bc770683de212f98d37c232c5
tannerlsmith <tanner.smith@uvm.edu>
2021-05-14 14:38:37 -0400

    good starting point

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,29 @@
 // Assignment code here
-var promptUpperCase = window.prompt("Would you like an uppercase character?") 
 
-var promptLowerCase = window.prompt("Would you like a lowercase character?")
-
-var promptNumber = window.prompt("Would you like random numbers?")
-
-var randomSymbols = prompt("Would you like symbols?")
-
-// Get references to the #generate element (This will grab an id of generate from the HTML)
-var generateBtn = document.querySelector("#generate")
-
-
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate"); // references dom
 
 // Write password to the #password input
-//calls a function called generatePassword that returns a user's final pass.
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword(); //calls generatePassword (not defined)
+  var passwordText = document.querySelector("#password"); //grabs password element
 
-  passwordText.value = password;
-
+  passwordText.value = password; //sets the value of generatePassword (change the value)
 }
+
+
 
 // Add event listener to generate button
-//adds event listener for click on the generateBtn and calls writepass function
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  var question1 = prompt('Type "yes" if you would like an uppercase letter');
+  if (question1 === 'yes') {
+    return ("CAPITAL LETTERS");
+  } else {
+    return ("blank");
+  }
 
 
 
-
-
-
-
-
-
-
-
-// generator functions 
-// assignments 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48); // selects number from 0-9 starting at 48
-}
-
-
-
-//1. how long user's pass. is?
-//2. lowercase chars?
-//3. uppercase chars?
-//4. numbers?
-//5. symbols?
