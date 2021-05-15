f2161e79f4319764825adf085bc6f2e0a82d6c29
tannerlsmith <tanner.smith@uvm.edu>
2021-05-15 15:47:05 -0400

    added all up to charLength

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,69 @@
 // Assignment code here
 var upperCase = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
 
 var lowerCase = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' 
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
 var numbers = [
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ];
 
+var special = [
+  '!', '#', '$', '%', '&', '+', '='
+]
+
+var passArray = []
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); // references dom
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); //calls generatePassword (not defined)
+  console.log()
+  var password = generatePassword() 
   var passwordText = document.querySelector("#password"); //grabs password element
   
   passwordText.value = password; //sets the value of generatePassword (change the value)
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
-  var charLength = prompt("How long would you like your password? (choose between 8 and 128 characters)")
-
-  var confirm1 = confirm ("Press ok for uppercase letters") 
-    if(confirm1) {
-      upperCase [Math.floor(Math.random() * upperCase.length - 1)]
-    }
-    
-
+  var charLength = prompt("How long would you like your password? (choose between 8 and 128 characters)");
+  
+  var confirm1 = confirm ("Press ok for uppercase letters"); 
+  if(confirm1) {
+    passArray.push(upperCase[Math.floor(Math.random() * upperCase.length - 1)]);
+    console.log(passArray)
+  } 
+  
   var confirm2 = confirm ("Press ok for lowercase letters")
-    if(confirm2) {
-      lowerCase [Math.floor(Math.random() * lowerCase.length - 1)]
-    } 
-
+  if(confirm2) {
+    passArray.push(lowerCase[Math.floor(Math.random() * lowerCase.length - 1)]);
+    console.log(passArray)
+  } 
+  
   var confirm3 = confirm ("Press ok for numbers")
-    if(confirm3) {
-      lowerCase [Math.floor(Math.random() * numbers - 1)]
-    } 
+  if(confirm3) {
+    passArray.push(numbers[Math.floor(Math.random() * numbers.length - 1)]);
+    console.log(passArray)
+  } 
+  
+  var confirm4 = confirm ("Press ok for special characters")
+  if(confirm4) {
+    passArray.push(special[Math.floor(Math.random() * special.length - 1)]);
+    console.log(passArray)
+  } 
+  
+  
+  //generate character length
+  //generate length of password based on the length of characterlength
+  //generatePassword()
+
+  
+  console.log(passArray)
 };
